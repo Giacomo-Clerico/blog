45e24159a8859cdc6677bcdce1006e09dabb1345
Giacomo <giacomo.clerico@outlook.it>
2025-06-22 13:12:36 +0200

    feat: add option to delete post from edit page with confirmation message

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post # blog post creation page
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post # singular post visualization page
   patch "/blog_posts/:id", to: "blog_posts#update" # patch request to update database entry
+  delete "blog_posts/:id", to: "blog_posts#destroy" # delete a post
   get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post # blog post edit page
   post "/blog_posts", to: "blog_posts#create", as: :blog_posts # post request to save to database
 
