74396238423f0bf138cd66043873d2aad484b0b5
Giacomo <giacomo.clerico@outlook.it>
2025-06-22 13:20:31 +0200

    refactor: moved all routes to just one builtin resource

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post # blog post creation page
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post # singular post visualization page
-  patch "/blog_posts/:id", to: "blog_posts#update" # patch request to update database entry
-  delete "blog_posts/:id", to: "blog_posts#destroy" # delete a post
-  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post # blog post edit page
-  post "/blog_posts", to: "blog_posts#create", as: :blog_posts # post request to save to database
+  resources :blog_posts
+
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
